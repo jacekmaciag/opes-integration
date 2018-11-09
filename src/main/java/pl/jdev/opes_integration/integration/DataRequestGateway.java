6d867b2e8f7f304653efefdf08249e56cd126f54
JDelorean <jacek.maciag@gmail.com>
2018-11-09 16:52:12 +0100

    Working channel!

--- src/main/java/pl/jdev/opes_integration/integration/DataRequestGateway.java
+++
@@ -1,5 +0,0 @@
-package pl.jdev.opes_integration.integration;
-
-public interface DataRequestGateway {
-    String echo(String request);
-}
--- src/main/java/pl/jdev/opes_integration/integration/service/IndicatorProviderService.java
+++
@@ -1,7 +0,0 @@
-package pl.jdev.opes_integration.integration.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class IndicatorProviderService {
-}
