b62e2da5dd746f7b02807ee3bced382276217321
jacek.maciag <jacek.maciag@gmail.com>
2020-09-06 16:05:29 +0200

    update project

--- src/main/java/pl/jdev/opes_integration/integration/OpesIntegration.java
+++ src/main/java/pl/jdev/opes_integration/integration/OpesIntegration.java
@@ -3,11 +3,14 @@ package pl.jdev.opes_integration.integration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@PropertySource(value = {"classpath:application.properties"})
 @ImportResource("integration-context.xml")
 public class OpesIntegration extends SpringBootServletInitializer {
 
